67e0284890430ffa7a3dd26d9e1d02e5e77f091c
Qubert <upafourth@gmail.com>
2017-12-01 10:47:30 +0100

    Initial code

--- CTCI 2.1 Remove Duplicates/Program.cs
+++ CTCI 2.1 Remove Duplicates/Program.cs
@@ -10,6 +10,18 @@ namespace CTCI_2._1_Remove_Duplicates
     {
         static void Main(string[] args)
         {
+            PrintHeaderMsg(2, 1, "Remove Duplicates");
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void PrintHeaderMsg(int chapter, int problem, string title)
+        {
+            Console.WriteLine("Cracking the Coding Interview");
+            Console.WriteLine("Chapter " + chapter + ", Problem " + chapter + "." + problem + ": " + title);
+            Console.WriteLine();
         }
     }
 }
